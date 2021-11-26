c8cd7ab3a15f01cb7233a0d82bf7d3968bb58abd
cmdenthusiant <53386624+cmdenthusiant@users.noreply.github.com>
2021-11-26 18:03:59 +0800

    Update s0urceBotnet.js

--- s0urceBotnet.js
+++ s0urceBotnet.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  A botnet for s0urce!
-// @author       HKquiet
+// @author       cmdenthusiant
 // @match        http://s0urce.io/
 // @icon         https://www.google.com/s2/favicons?domain=s0urce.io
 // @grant        GM_setValue
